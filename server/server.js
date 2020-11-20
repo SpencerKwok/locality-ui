f45ce0c2a4366df8c5453525eb1845e8edcd2ad2
Spencer Kwok <spencer.kwok16@gmail.com>
2020-11-19 21:35:49 -0500

    Trying again...

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, '../build/index.html'));
 });
 
 port = process.env.PORT || 3001
