48aebfbafe7601bf924ff165369afa10ecddaf0f
Spencer Kwok <spencer.kwok16@gmail.com>
2020-11-19 21:42:56 -0500

    Moving express.static func to the top of server.js for faster loading of page

--- server/server.js
+++ server/server.js
@@ -3,9 +3,9 @@ const path = require('path');
 const bodyParser = require("body-parser");
 
 const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../build/index.html'));
