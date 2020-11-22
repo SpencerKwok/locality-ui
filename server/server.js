1e7acc34c96d2eb49f3943eabfa2c929839da6b5
Spencer Kwok <spencer.kwok16@gmail.com>
2020-11-21 22:15:28 -0500

    Adding notification form + updating package.json to npm-shrinkwrap.json + adding typescript

--- server/server.js
+++ server/server.js
@@ -1,17 +1,18 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const bodyParser = require("body-parser");
+const http = require("http");
+const port = process.env.PORT || 3001;
 
 const app = express();
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "../build")));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../build/index.html"));
+});
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../build/index.html'));
-});
-
-port = process.env.PORT || 3001
-app.listen(port, () => {
-    console.log('Server listening on port::' + port);
+const server = http.createServer(app);
+server.listen(port, () => {
+  console.log("Server listening on port::" + port);
 });
