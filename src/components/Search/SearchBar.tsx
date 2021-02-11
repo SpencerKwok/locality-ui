be9e6b7b2c963c6a9eeb9112823f8e2123c26f14
Spencer Kwok <spencer.kwok16@gmail.com>
2021-02-11 13:19:39 -0800

    Fix compatability with older browsers

--- src/components/Search/SearchBar.tsx
+++ src/components/Search/SearchBar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 
 import Stack from "../Stack/Stack";
-import { Button, InputGroup, FormControl } from "react-bootstrap";
+import { Button, InputGroup } from "react-bootstrap";
 
 export interface SearchBarProps extends React.HTMLProps<HTMLInputElement> {
   onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
@@ -20,9 +20,25 @@ const StyledButton = styled(Button)`
   padding: 11px;
   background-color: #449ed7;
   border-color: #449ed7;
-  :hover {
-    background-color: #3880ae;
-    border-color: #3880ae;
+  &:link {
+    background-color: #449ed7 !important;
+    border-color: #449ed7 !important;
+  }
+  &:visited {
+    background-color: #449ed7 !important;
+    border-color: #449ed7 !important;
+  }
+  &:focus {
+    background-color: #449ed7 !important;
+    border-color: #449ed7 !important;
+  }
+  &:hover {
+    background-color: #3880ae !important;
+    border-color: #3880ae !important;
+  }
+  &:active {
+    background-color: #3880ae !important;
+    border-color: #3880ae !important;
   }
 `;
 
@@ -34,7 +50,8 @@ function SearchBar(props: SearchBarProps) {
       style={{ marginLeft: (props.width as number) * 0.07 }}
     >
       <StyledInputGroup size="lg" width={props.width}>
-        <FormControl
+        <input
+          className="form-control"
           autoFocus
           aria-label="Large"
           aria-describedby="inputGroup-sizing-sm"
