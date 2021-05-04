cb1c0c942df6e2394b48fedf857572282d671301
Spencer Kwok <spencer.kwok16@gmail.com>
2021-05-04 16:22:50 -0700

    Adding brotli compression

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
+const zlib = require("zlib");
+
 module.exports = {
+  compress: {
+    br: function () {
+      return zlib.createBrotliCompress({
+        params: {
+          [zlib.constants.BROTLI_PARAM_QUALITY]:
+            zlib.constants.BROTLI_MIN_QUALITY,
+        },
+      });
+    },
+    gzip: function () {
+      return zlib.createGzip({ level: zlib.constants.Z_BEST_SPEED });
+    },
+  },
   redirects: async () => {
     return [
       {
