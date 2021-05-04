d81ff037101398b437382ccff7a2c3ef7e2f15b8
Spencer Kwok <spencer.kwok16@gmail.com>
2021-05-04 14:40:55 -0700

    Adding privacy + deletion policies

---
+++ pages/deletion/index.tsx
@@ -0,0 +1,15 @@
+import RootLayout from "../../components/common/RootLayout";
+
+export default function Privacy() {
+  return (
+    <RootLayout>
+      <div style={{ marginLeft: 12 }}>
+        <h1>User Data Deletion Instructions</h1>
+        <p>
+          Contact Locality at locality.info@yahoo.com with the account's email
+          with the subject "Delete My Account" to delete the account
+        </p>
+      </div>
+    </RootLayout>
+  );
+}
