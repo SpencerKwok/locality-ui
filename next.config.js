fca18da2bca14081690d1b22f000962d3cb32786
Spencer Kwok <spencer.kwok16@gmail.com>
2021-05-04 19:18:23 -0700

    Adding secure headers to app

--- next.config.js
+++ next.config.js
@@ -1,6 +1,51 @@
 const zlib = require("zlib");
+const { createSecureHeaders } = require("next-secure-headers");
 
 module.exports = {
+  headers: async () => {
+    return [
+      {
+        source: "/(.*)",
+        headers: createSecureHeaders({
+          contentSecurityPolicy: {
+            directives: {
+              childSrc: ["'self'"],
+              connectSrc: [
+                "'self'",
+                "https://ipv4.icanhazip.com",
+                "https://api.ipify.org",
+                "https://api.cloudinary.com",
+              ],
+              defaultSrc: ["'self'"],
+              fontSrc: ["'self'"],
+              frameSrc: ["'self'"],
+              imgSrc: [
+                "'self'",
+                "blob:",
+                "data:",
+                "https://res.cloudinary.com",
+              ],
+              manifestSrc: ["'self'"],
+              mediaSrc: ["'self'", "https://res.cloudinary.com"],
+              prefetchSrc: ["'self'"],
+              objectSrc: ["'self'"],
+              scriptSrc: ["'self'"],
+              scriptSrcElem: ["'self'"],
+              scriptSrcAttr: ["'self'"],
+              styleSrc: ["'self'", "'unsafe-inline'"],
+              styleSrcElem: ["'self'", "'unsafe-inline'"],
+              styleSrcAttr: ["'self'", "'unsafe-inline'"],
+              workerSrc: ["'self'"],
+            },
+          },
+          referrerPolicy: "same-origin",
+          xssProtection: "block-rendering",
+          "Permissions-Policy":
+            "accelerometer=(), autoplay=(), camera=(), document-domain=(), encrypted-media=(), fullscreen=(), geolocation=(), gyroscope=(), magnetometer=(), microphone=(), midi=(), payment=(), picture-in-picture=(), publickey-credentials-get=(), sync-xhr=(), usb=(), screen-wake-lock=(), xr-spatial-tracking=()",
+        }),
+      },
+    ];
+  },
   compress: {
     br: function () {
       return zlib.createBrotliCompress({
