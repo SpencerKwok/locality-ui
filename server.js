9b4278b1b3506a771d33343ef813fe0630531a64
Spencer Kwok <spencer.kwok16@gmail.com>
2021-05-07 10:50:15 -0700

    Removing helmet to reduce overhead on API requests

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ const fs = require("fs");
 const next = require("next");
 const { parse } = require("url");
 
-const helmet = require("helmet");
 const shrinkRay = require("shrink-ray-current");
 
 const app = next({ dev: process.env.NODE_ENV !== "production" });
@@ -20,68 +19,6 @@ const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   const server = express();
-
-  server.use(
-    helmet({
-      contentSecurityPolicy: {
-        directives: {
-          childSrc: ["'self'"],
-          connectSrc: [
-            "'self'",
-            "https://ipv4.icanhazip.com",
-            "https://api.ipify.org",
-            "https://api.cloudinary.com",
-          ],
-          defaultSrc: ["'self'"],
-          fontSrc: ["'self'"],
-          frameSrc: ["'self'"],
-          imgSrc: ["'self'", "blob:", "data:", "https://res.cloudinary.com"],
-          manifestSrc: ["'self'"],
-          mediaSrc: ["'self'", "https://res.cloudinary.com"],
-          prefetchSrc: ["'self'"],
-          objectSrc: ["'self'"],
-          scriptSrc: ["'self'"],
-          scriptSrcElem: ["'self'"],
-          scriptSrcAttr: ["'self'"],
-          styleSrc: ["'self'", "'unsafe-inline'"],
-          styleSrcElem: ["'self'", "'unsafe-inline'"],
-          styleSrcAttr: ["'self'", "'unsafe-inline'"],
-          workerSrc: ["'self'"],
-        },
-      },
-
-      // Need to be able to load images from Cloudinary
-      crossOriginEmbedderPolicy: false,
-      crossOriginOpenerPolicy: false,
-      crossOriginResourcePolicy: false,
-
-      dnsPrefetchControl: {
-        // Less privacy for users, but improves performance
-        allow: false,
-      },
-      expectCt: {
-        enforce: true,
-      },
-      frameguard: {
-        action: "same-origin",
-      },
-      hsts: {
-        includeSubDomains: true,
-        maxAge: 31536000,
-        preload: true,
-      },
-      ieNoOpen: true,
-      noSniff: true,
-      originAgentCluster: true,
-      permittedCrossDomainPolicies: {
-        permittedPolicies: "none",
-      },
-      referrerPolicy: {
-        policy: "same-origin",
-      },
-      xssFilter: true,
-    })
-  );
 
   server.use(
     shrinkRay({
