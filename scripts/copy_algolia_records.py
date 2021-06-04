27eb0ca1274959dbaf8456314bf9608922334c18
Spencer Kwok <spencer.kwok16@gmail.com>
2021-06-03 20:05:28 -0700

    Remove API keys from script lol

--- scripts/copy_algolia_records.py
+++ scripts/copy_algolia_records.py
@@ -2,6 +2,6 @@ from algoliasearch.search_client import SearchClient
 from algoliasearch.account_client import AccountClient
 
 # Copy from SRC to DST
-src_index = SearchClient.create('X5610KET2K', '1dcfc4ee43ef0de65c93ea661524a425').init_index('locality_prod')
-dst_index = SearchClient.create('HJ20MIEM9K', '8224a01c8bda9a91a33cfb6c2abf0a47').init_index('dummy')
+src_index = SearchClient.create('SRC_APP_ID', 'SRC_API_KEY').init_index('SRC_INDEX')
+dst_index = SearchClient.create('DST_APP_ID', 'DST_API_KEY').init_index('DST_INDEX')
 AccountClient.copy_index(src_index, dst_index)#, { 'scope': ['settings', 'synonyms'] })
