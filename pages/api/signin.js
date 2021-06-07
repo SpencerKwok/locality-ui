a09b815d7d6a6e13d778eeb587990b30b5e55e71
Spencer Kwok <spencer.kwok16@gmail.com>
2021-06-06 18:53:51 -0700

    oops

--- pages/api/signin.js
+++ pages/api/signin.js
@@ -23,13 +23,13 @@ export default async function handler(req, res) {
   }
 
   const response = await signIn("credentials", {
-    email: values.email,
-    password: values.password,
+    email: email,
+    password: password,
     redirect: false,
   });
 
   console.log(response);
   console.log(req.headers);
 
-  res.status(200).end();
+  res.status(200).json(req.headers);
 }
