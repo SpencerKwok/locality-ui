9d0cb2a8c3ffacdab84fa0f3f9cf8e7554a3c679
Spencer Kwok <spencer.kwok16@gmail.com>
2021-06-06 21:10:45 -0700

    Using uid for extension login

---
+++ pages/api/extension/analytics/event.js
@@ -0,0 +1,50 @@
+import Xss from "xss";
+
+import {
+  GOOGLE_ANALYTICS_API_SECRET,
+  GOOGLE_MEASUREMENT_ID,
+} from "../../../../lib/env";
+
+export default async function handler(req, res) {
+  if (req.method !== "GET") {
+    res.status(400).json({ error: "Must be GET method" });
+    return;
+  }
+
+  // Redirect users immediately, users should
+  // not wait on analytics to complete
+  const redirectUrl = Xss(decodeURIComponent(req.query["redirect_url"] || ""));
+  if (redirectUrl) {
+    res.redirect(redirectUrl);
+  } else {
+    res.status(200).end();
+  }
+
+  // If there are any issues with
+  // the sent event, ignore it
+  const name = Xss(decodeURIComponent(req.query["name"] || ""));
+  if (!name) {
+    return;
+  }
+
+  // Don't send name and redirect url to Google analytics
+  req.query["name"] = undefined;
+  req.query["redirect_url"] = undefined;
+  fetch(
+    `https://www.google-analytics.com/mp/collect?measurement_id=${GOOGLE_MEASUREMENT_ID}&api_secret=${GOOGLE_ANALYTICS_API_SECRET}`,
+    {
+      method: "POST",
+      body: JSON.stringify({
+        client_id: "extension",
+        events: [
+          {
+            name,
+            params: req.query,
+          },
+        ],
+      }),
+    }
+  ).catch((err) => {
+    console.log(err);
+  });
+}
---
+++ pages/api/extension/wishlist/get.js
@@ -0,0 +1,56 @@
+import { camelCase, mapKeys } from "lodash";
+import SqlString from "sqlstring";
+
+import Algolia from "../../../../lib/api/algolia";
+import Psql from "../../../../lib/api/postgresql";
+import { runMiddlewareExtension } from "../../../../lib/api/middleware";
+
+export default async function handler(req, res) {
+  await runMiddlewareExtension(req, res);
+
+  if (req.method !== "GET") {
+    res.status(400).json({ error: "Must be GET method" });
+    return;
+  }
+
+  const { id } = req.locals.user;
+  const [productIDs, productIDsError] = await Psql.query(
+    SqlString.format("SELECT wishlist FROM users WHERE id=?", [id])
+  );
+  if (productIDsError) {
+    res.status(500).json({ error: productIDsError });
+    return;
+  }
+
+  const wishlist = productIDs.rows[0].wishlist
+    .split(",")
+    .filter(Boolean)
+    .map((id) => ({
+      objectId: id.split("_").slice(0, 2).join("_"),
+      variantIndex: id.split("_")[2],
+    }));
+
+  const [products, productsError] = await Algolia.getObjects(
+    wishlist.map(({ objectId }) => objectId),
+    {
+      attributesToRetrieve: ["objectId"],
+    }
+  );
+  if (productsError) {
+    res.status(500).json({ error: productsError });
+    return;
+  }
+
+  const results = [];
+  for (let i = 0; i < wishlist.length; ++i) {
+    if (!products[i]) {
+      continue;
+    }
+    results.push({
+      ...mapKeys(products[i], (v, k) => camelCase(k)),
+      variantIndex: wishlist[i].variantIndex,
+    });
+  }
+
+  res.status(200).json({ products: results });
+}
