63f0ae67771370bc984d42c15a9acc01cd2d222c
Spencer Kwok <spencer.kwok16@gmail.com>
2021-06-19 11:51:34 -0700

    Print errors on console when logging to sumologic

--- lib/api/sumologic.tsx
+++ lib/api/sumologic.tsx
@@ -5,11 +5,16 @@ const sumoLogicClient: {
   log: (params: Message) => void;
 } = {
   log: ({ level, message, method, params }) => {
+    const err = `${Date()} level=${level} message=${message} ${
+      method ? `method=${method}` : ""
+    } ${params ? `params=${JSON.stringify(params)}` : ""}`;
+
+    // Log to console for real-time debugging
+    console.log(err);
+
     fetch(url, {
       method: "POST",
-      body: `${Date()} level=${level} message=${message} ${
-        method ? `method=${method}` : ""
-      } ${params ? `params=${JSON.stringify(params)}` : ""}`,
+      body: err,
     }).catch((error) => {
       // Sumologic is our log console, so if logging
       // fails at least output an error to Heroku console
