09d750d17251bf94ee6356a7e6eb8b61f9682776
Spencer Kwok <spencer.kwok16@gmail.com>
2021-08-04 00:28:43 -0400

    Oops

--- components/home/Home.tsx
+++ components/home/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import dynamic from "next/dynamic";
 import styled from "styled-components";
