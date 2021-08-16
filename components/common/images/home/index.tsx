bfbe27f5ab5d3750ad783f629ed4d48b74c97afd
Spencer Kwok <spencer.kwok16@gmail.com>
2021-08-15 20:10:38 -0400

    Fix build

--- components/common/images/home/index.tsx
+++ components/common/images/home/index.tsx
@@ -1,4 +1,3 @@
-export { default as LocalityLogo } from './LocalityLogo'
 export { default as OurMission } from './OurMission'
 export { default as OurPurpose } from './OurPurpose'
 export { default as OurVision } from './OurVision'
