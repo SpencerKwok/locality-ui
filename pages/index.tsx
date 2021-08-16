af31d4950cee887a10996b9c4a50ce9b4bd55dbb
Spencer Kwok <spencer.kwok16@gmail.com>
2021-08-15 21:59:02 -0400

    Make homepage static generated

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,9 @@ import { useRouter } from "next/router";
 
 import HomePage from "components/home/Home";
 import RootLayout from "components/common/RootLayout";
-import SumoLogic from "lib/api/sumologic";
 import { useWindowSize } from "lib/common";
 
-import { helper } from "./api/businesses";
-
 import type { FC } from "react";
-import type { GetServerSideProps } from "next";
 import type { BaseBusiness } from "../common/Schema";
 import type { Session } from "next-auth";
 
@@ -16,48 +12,6 @@ interface HomeProps {
   businesses: Array<BaseBusiness>;
   session: Session | null;
 }
-
-let cachedBusinesses: Array<{
-  id: number;
-  logo: string;
-  homepages: { homepage: string };
-  name: string;
-}> = [];
-export const getServerSideProps: GetServerSideProps = async () => {
-  await new Promise((resolve) => {
-    void helper().then((res) => {
-      if (!res) {
-        // Don't update cached businesses
-        // if we fail to fetch them
-        SumoLogic.log({
-          level: "error",
-          method: "/",
-          message: "Failed to fetch businesses",
-        });
-        resolve(false);
-        return;
-      }
-      cachedBusinesses = res.businesses
-        .map(({ id, logo, homepages, name }) => ({
-          id,
-          logo,
-          homepages: { homepage: homepages.homepage },
-          name,
-        }))
-        .sort((a: { id: number }, b: { id: number }) => b.id - a.id);
-      resolve(true);
-    });
-
-    if (cachedBusinesses.length > 0) {
-      resolve(true);
-    }
-  });
-  return {
-    props: {
-      businesses: cachedBusinesses,
-    },
-  };
-};
 
 const Home: FC<HomeProps> = ({ session }) => {
   const { query } = useRouter();
