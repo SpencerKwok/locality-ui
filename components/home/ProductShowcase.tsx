7e642f59dbf8db656003781d1f7e672e09b8e421
Spencer Kwok <spencer.kwok16@gmail.com>
2021-08-21 11:50:56 -0400

    Fix product scrolling regardless of scaling

--- components/home/ProductShowcase.tsx
+++ components/home/ProductShowcase.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { forwardRef, useRef, useState } from "react";
 
 import LeftArrow from "components/common/images/home/LeftArrow";
 import RightArrow from "components/common/images/home/RightArrow";
@@ -16,53 +16,50 @@ interface ProductLogoProps {
   loading?: "eager" | "lazy";
   alt: string;
   details: string;
-  id: string;
   href: string;
   src: string;
 }
 
-const ProductLogo: FC<ProductLogoProps> = ({
-  loading,
-  alt,
-  details,
-  id,
-  href,
-  src,
-}) => {
-  const [useFallback, setUseFallback] = useState(false);
-  return (
-    <a
-      href={href}
-      target="_blank"
-      rel="noopener noreferrer"
-      style={{
-        textDecoration: "none",
-        color: "#000000",
-      }}
-    >
-      <div className={styles["product-container-container"]}>
-        <div id={id} className={styles["product-container"]}>
-          <img
-            alt={alt}
-            loading={loading}
-            height={380}
-            width={281}
-            src={useFallback ? src.replace(".webp", ".jpg") : src}
-            onError={(): void => {
-              if (!useFallback) {
-                setUseFallback(true);
-              }
-            }}
-          />
+const ProductLogo = forwardRef<HTMLDivElement, ProductLogoProps>(
+  ({ loading, alt, details, href, src }, ref) => {
+    const [useFallback, setUseFallback] = useState(false);
+    return (
+      <a
+        href={href}
+        target="_blank"
+        rel="noopener noreferrer"
+        style={{
+          textDecoration: "none",
+          color: "#000000",
+        }}
+      >
+        <div className={styles["product-container-container"]} ref={ref}>
+          <div className={styles["product-container"]}>
+            <img
+              alt={alt}
+              loading={loading}
+              height={380}
+              width={281}
+              src={useFallback ? src.replace(".webp", ".jpg") : src}
+              onError={(): void => {
+                if (!useFallback) {
+                  setUseFallback(true);
+                }
+              }}
+            />
+          </div>
         </div>
-      </div>
-      <h3>{details}</h3>
-    </a>
-  );
-};
+        <h3>{details}</h3>
+      </a>
+    );
+  }
+);
 
 const ProductShowcase: FC<ProductShowcaseProps> = ({ loading, width }) => {
   const [arrowOpacity, setArrowOpacity] = useState([0.3, 1]);
+  const firstProductRef = useRef<HTMLDivElement>(null);
+  const lastProductRef = useRef<HTMLDivElement>(null);
+
   return (
     <Stack
       direction="column"
@@ -81,9 +78,9 @@ const ProductShowcase: FC<ProductShowcaseProps> = ({ loading, width }) => {
       >
         <ProductLogo
           loading={loading}
+          ref={firstProductRef}
           alt="Local Goodie 1"
           details="10 Snack Box"
-          id="local-goodie-1"
           href="https://laidbacksnacks.com"
           src="https://res.cloudinary.com/hcory49pf/image/upload/v1629517880/home/local-goodie-1.webp"
         />
@@ -91,7 +88,6 @@ const ProductShowcase: FC<ProductShowcaseProps> = ({ loading, width }) => {
           loading={loading}
           alt="Local Goodie 2"
           details="Herbal Bath Tea No 2: Ease"
-          id="local-goodie-2"
           href="https://www.liveeasyco.ca"
           src="https://res.cloudinary.com/hcory49pf/image/upload/v1629517460/home/local-goodie-2.webp"
         />
@@ -99,7 +95,6 @@ const ProductShowcase: FC<ProductShowcaseProps> = ({ loading, width }) => {
           loading={loading}
           alt="Local Goodie 3"
           details="Bath and Body Aromatherapy Oil"
-          id="local-goodie-3"
           href="https://shopcadine.com"
           src="https://res.cloudinary.com/hcory49pf/image/upload/v1629517499/home/local-goodie-3.webp"
         />
@@ -107,15 +102,14 @@ const ProductShowcase: FC<ProductShowcaseProps> = ({ loading, width }) => {
           loading={loading}
           alt="Local Goodie 4"
           details="Chonky (Sharky) Printed T-Shirt"
-          id="local-goodie-4"
           href="https://www.unimpressedofficial.com"
           src="https://res.cloudinary.com/hcory49pf/image/upload/v1629517044/home/local-goodie-4.webp"
         />
         <ProductLogo
           loading={loading}
+          ref={lastProductRef}
           alt="Local Goodie 5"
           details="12-pack Custom Assorted Case"
-          id="local-goodie-5"
           href="https://www.talitykombucha.com"
           src="https://res.cloudinary.com/hcory49pf/image/upload/v1629519473/home/local-goodie-5.webp"
         />
@@ -130,30 +124,24 @@ const ProductShowcase: FC<ProductShowcaseProps> = ({ loading, width }) => {
           opacity={arrowOpacity[0]}
           style={{ cursor: "pointer" }}
           onClick={(): void => {
-            const elem = document.getElementById("local-goodie-1");
-            if (elem) {
-              elem.scrollIntoView({
-                behavior: "smooth",
-                block: "center",
-                inline: "center",
-              });
-              setArrowOpacity([0.3, 1]);
-            }
+            firstProductRef.current?.scrollIntoView({
+              behavior: "smooth",
+              block: "nearest",
+              inline: "end",
+            });
+            setArrowOpacity([0.3, 1]);
           }}
         />
         <RightArrow
           opacity={arrowOpacity[1]}
           style={{ cursor: "pointer" }}
           onClick={(): void => {
-            const elem = document.getElementById("local-goodie-5");
-            if (elem) {
-              elem.scrollIntoView({
-                behavior: "smooth",
-                block: "center",
-                inline: "center",
-              });
-              setArrowOpacity([1, 0.3]);
-            }
+            lastProductRef.current?.scrollIntoView({
+              behavior: "smooth",
+              block: "nearest",
+              inline: "end",
+            });
+            setArrowOpacity([1, 0.3]);
           }}
         />
       </Stack>
