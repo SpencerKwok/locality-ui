be15ed2b122f8de1e4fc92829423e5c5e4aa1e5f
Spencer Kwok <spencer.kwok16@gmail.com>
2021-11-09 21:14:56 -0500

    Lint

--- background/lib/sumologic.py
+++ background/lib/sumologic.py
@@ -3,12 +3,21 @@ import os
 from datetime import datetime
 from sumologic import SumoLogic
 
-sumo = SumoLogic(os.environ["SUMOLOGIC_ACCESS_ID"], os.environ["SUMOLOGIC_ACCESS_KEY"], endpoint=os.environ["SUMOLOGIC_URL"])
+sumo = SumoLogic(
+    os.environ["SUMOLOGIC_ACCESS_ID"],
+    os.environ["SUMOLOGIC_ACCESS_KEY"],
+    endpoint=os.environ["SUMOLOGIC_URL"],
+)
+
+
 def post(level: str, message: str, method: str = None, params: dict[str, str] = None):
-    sumo.post("", { 
-        "date": datetime.now().strftime("%m/%d/%Y, %H:%M:%S"),
-        "level": level,
-        "message": message,
-        "method": method,
-        "params": params
-    })
+    sumo.post(
+        "",
+        {
+            "date": datetime.now().strftime("%m/%d/%Y, %H:%M:%S"),
+            "level": level,
+            "message": message,
+            "method": method,
+            "params": params,
+        },
+    )
